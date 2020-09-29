2f1146340c7eab316fcd83263369a7b60b880bbf
Morozov312 <alexander.morozov312@gmail.com>
2020-09-30 03:33:45 +0700

    on list

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
 #include "list.hpp"
 #include "record.hpp"
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 forwardList readFile(char *path) {
-    forwardList list;
+    forwardList tempList;
     FILE *file;
     file = fopen(path, "rb");
-    record arrayRecords[4000] = {0};
-    fread((record *) arrayRecords, sizeof(record), 4000, file);
-    for (auto &i : arrayRecords) {
-        cout << i.author <<endl;
-        cout << i.title <<endl;
-        cout << i.publishingHouse <<endl;
-        cout << i.year <<endl;
-        cout << i.qtyPages <<endl;
-        cout<<endl;
+    record temp{};
+    for (int i = 0; i < 4000; ++i) {
+        fread(&temp,sizeof(record),1,file);
+        tempList.addNode(temp);
+        fseek(file,sizeof(record),1);
     }
     fclose(file);
-    return list;
+    return tempList;
 }
 int main() {
     char path[] = "testBase1.dat";
-    readFile(path);
+    forwardList list = readFile(path);
+    list.printList();
     return 0;
 }
